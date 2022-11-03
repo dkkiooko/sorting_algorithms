72688fa94aa77d8b5e2820a4205e3d70c3f9f28d
dkkiooko <kithekadankioko@gmail.com>
2022-11-03 09:58:38 +0300

    inital commit with the bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+* bubble_sort - bubble sort algorithm
+* @array: array of integers
+* @size: length of array
+* Return: nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp = 0;
+
+	if (!array)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;	
+				print_array(array, size);
+			}
+		}
+	}
+
+}
